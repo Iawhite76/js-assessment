daaca3ee8ff91bbf2e1687fc511a08d5c993dca1
Ian White <ianwhite@utexas.edu>
2014-03-02 16:38:00 -0600

    begin test 1 for count

--- app/count.js
+++ app/count.js
@@ -3,6 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var count=start;
+
+      var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+      function timer()
+          {
+            count=count+1;
+            if (count >= end)
+            {
+               clearInterval(counter);
+               //counter ended, do something here
+               return;
+            } else {
+                 console.log(count);
+              }
+             //Do code for showing the number of seconds here
+            }
     }
   };
 });
