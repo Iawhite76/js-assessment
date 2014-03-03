b9f5d117d7cc4aeb7283fce0ce47f3ccbb0bff48
Ian White <ianwhite@utexas.edu>
2014-03-03 12:20:21 -0600

    pass 1st test regex

--- app/regex.js
+++ app/regex.js
@@ -3,7 +3,13 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        var patt1=/\d/;
+        return patt1.test(str);
+        // if (result == true ) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     },
 
     containsRepeatingLetter : function(str) {
