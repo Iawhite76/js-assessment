0601cc9346dfbb29f3818963c1cdb2ab9e154f99
Ian White <ianwhite@utexas.edu>
2014-03-04 11:07:06 -0600

    drew and patricks answer for tests

--- app/numbers.js
+++ app/numbers.js
@@ -4,6 +4,17 @@ define(function() {
   return {
     valueAtBit: function(num, bit) {
 
+        // Drew's solution
+        // var n = num.toString(2);  convert number to binary string (toString(2))
+        // var arr = n.split(' ');
+        // var newBit = arr.length - bit;
+        // var number = arr[newBit];
+        // var value = parseInt(number, 10);
+        // return value;
+
+        // patrick's solutions
+        // binaryNum = this.convertToBinary.split(' ').reverse().join(' ');
+        // return Number(binaryNum.slice(bit - 1, bit));
     },
 
     base10: function(str) {
