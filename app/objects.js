16b7ded0ab33f2c8c467dd3036f2109434bae100
Ian White <ianwhite@utexas.edu>
2014-03-19 15:15:57 -0500

    pass first test under objects and context

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      obj.func = fn;
+     return obj.func();
     },
 
     alterObjects : function(constructor, greeting) {
